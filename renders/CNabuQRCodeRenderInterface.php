bda47a4eece295270cb72e8795ad9eab6d5e0779
Rafael Gutiérrez Martínez <ragutimar@gmail.com>
2018-08-25 11:05:41 +0200

    Add parametric methods to manage Render from external code

--- renders/CNabuQRCodeRenderInterface.php
+++ renders/CNabuQRCodeRenderInterface.php
@@ -27,6 +27,8 @@ use BaconQrCode\Renderer\Image\SvgImageBackEnd;
 use BaconQrCode\Renderer\RendererStyle\RendererStyle;
 use BaconQrCode\Writer;
 
+use nabu\render\exceptions\ENabuRenderException;
+
 /**
  * Class to Render a QR Code as an HTTP Response.
  * @author Rafael Gutierrez <rgutierrez@nabu-3.com>
@@ -38,6 +40,8 @@ class CNabuQRCodeRenderInterface extends CNabuRenderInterfaceAdapter
 {
     public function init()
     {
+        $this->setSquareSize(400);
+
         return true;
     }
 
@@ -48,11 +52,64 @@ class CNabuQRCodeRenderInterface extends CNabuRenderInterfaceAdapter
 
     public function render()
     {
+        switch ($this->mimetype) {
+            case 'image/svg+xml':
+                $backend = new SvgImageBackEnd();
+                break;
+        }
+
+        if (!isset($backend)) {
+            throw new ENabuRenderException(ENabuRenderException::ERROR_INVALID_MIME_TYPE, array($this->mimetype));
+        }
+
         $renderer = new ImageRenderer(
-            new RendererStyle(400),
-            new SvgImageBackEnd()
+            new RendererStyle($this->getSquareSize()),
+            new $backend
         );
         $writer = new Writer($renderer);
-        echo $writer->writeString('Copyright (c) 2018 nabu-3 Group');
+        $content = $this->getValue('content');
+        echo $writer->writeString(is_string($content) ? $content : 'Copyright (c) 2018 nabu-3 Group');
+    }
+
+    /**
+     * Gets the QR Content.
+     * @return string|null Returns the content of the QR or null if not setted.
+     */
+    public function getContent()
+    {
+        return $this->getValue('content');
+    }
+
+    /**
+     * Set or reset the QR Content.
+     * @param  string|null $content The content to be setted or null to reset content.
+     * @return CNabuQRCodeRenderInterface Returns the self pointer to grant chained setter calls.
+     */
+    public function setContent(string $content = null) : CNabuQRCodeRenderInterface
+    {
+        $this->setValue('content', $content);
+
+        return $this;
+    }
+
+    /**
+     * Gets the QR Square Size.
+     * @return int Returns current Square Size.
+     */
+    public function getSquareSize() : int
+    {
+        return $this->getValue('square_size');
+    }
+
+    /**
+     * Set the QR Square Size.
+     * @param  int $size The size to be setted.
+     * @return CNabuQRCodeRenderInterface Returns the self pointer to grant chained setter calls.
+     */
+    public function setSquareSize(int $size) : CNabuQRCodeRenderInterface
+    {
+        $this->setValue('square_size', $size);
+
+        return $this;
     }
 }
